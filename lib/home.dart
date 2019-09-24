1def2fc07357ca78c22fe22ccd2ea21538827806
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-23 21:56:58 -0300

    Fetching data from API using Future

--- lib/home.dart
+++ lib/home.dart
@@ -11,11 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  // List _comidas = ["Nutella","mousse de chocolate","Sorvete","Açaí","Bag Burger - Easy Cheese","Outback","Pizza Pepperoni","Chocolate","Burger King","McDonalds","Lasanha","Nhoque","Duke n Duke"];
-
   String urlBase = "https://jsonplaceholder.typicode.com";
 
-  Future<List<Post>> _posts() async {
+  Future<List<Post>> _recuperarPosts() async {
+
+    print("Recuperar posts");
+
     http.Response response = await http.get(urlBase + "/posts");
     var dadosJson = json.decode( response.body );
 
@@ -35,122 +36,101 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Listas"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _posts(),
-        builder: (context, snapshot) {
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Atualizar"),
+                  onPressed: () {
+                    setState(() => {}); // O setState aqui faz o loading aparecer sempre que clico pra atualizar
+                    _recuperarPosts();
+                  }
+                ),
+                RaisedButton(
+                  child: Text("Postar"),
+                  onPressed: () => print("postar"),
+                ),
+                RaisedButton(
+                  child: Text("Remover"),
+                  onPressed: () => print("remover"),
+                ),
+              ],
+            ),
 
-          switch ( snapshot.connectionState ) {
-            case ConnectionState.active :
-            case ConnectionState.none :
-            case ConnectionState.waiting :
-              return Center(child: CircularProgressIndicator());
-              break;
+            Expanded(
+              child: FutureBuilder<List<Post>>(
+                future: _recuperarPosts(),
+                builder: (context, snapshot) {
 
-            case ConnectionState.done :
-              if (snapshot.hasError) {
-                print("Erro ao carregar os dados: " + snapshot.error.toString());
-                return Center(child: Text("Erro"));
-              }
+                  switch ( snapshot.connectionState ) {
+                    case ConnectionState.active :
+                    case ConnectionState.none :
+                    case ConnectionState.waiting :
+                      return Center(child: CircularProgressIndicator());
+                      break;
 
-            break;
-          }
+                    case ConnectionState.done :
+                      if (snapshot.hasError) {
+                        print("Erro ao carregar os dados: " + snapshot.error.toString());
+                        return Center(child: Text("Erro"));
+                      }
 
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) {
+                    break;
+                  }
 
-              List<Post> lista = snapshot.data;
-              Post post = lista[index];
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
 
-              return ListTile(
-                title: Text( post.title ),
-                subtitle: Text( post.id.toString() ),
-                onTap: () {
-                  
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text("Are you sure?"),
-                        content: Text( post.title ),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text("Cancelar"),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          RaisedButton(
-                            child: Text("Ok", style: TextStyle(color: Colors.white),),
-                            color: Colors.blue,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          )
-                        ],
+                      List<Post> lista = snapshot.data;
+                      Post post = lista[index];
+
+                      return ListTile(
+                        title: Text( post.title ),
+                        subtitle: Text( post.id.toString() ),
+                        onTap: () {
+                          
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Are you sure?"),
+                                content: Text( post.title ),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text("Cancelar"),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                  RaisedButton(
+                                    child: Text("Ok", style: TextStyle(color: Colors.white),),
+                                    color: Colors.blue,
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  )
+                                ],
+                              );
+                            }
+                          );
+
+                        },
                       );
+
                     }
                   );
 
                 },
-              );
+              ),
+            ),
 
-            }
-          );
-
-        },
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-/*
-Container(
-  child: ListView.builder(
-    itemCount: _comidas.length,
-    itemBuilder: (context, indice) {
-
-      return ListTile(
-        title: Text("Indice " + _comidas[indice]),
-        subtitle: Text("Comidas que eu adoro"),
-        onTap: () {
-          print("Quero " + _comidas[indice]);
-
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("Confirmar pedido de:"),
-                content: Text( _comidas[indice] ),
-                actions: <Widget>[
-
-                  FlatButton(
-                    child: Text("NÃO"),
-                    onPressed: () {
-                      print("Clicou NÃO para " + _comidas[indice]);
-                      Navigator.pop(context);
-                    }
-                  ),
-                  RaisedButton(
-                    child: Text("Sim", style: TextStyle(color: Colors.white),),
-                    onPressed: () {
-                      print("Clicou SIM para " + _comidas[indice]);
-                      Navigator.pop(context);
-                    },
-                    color: Colors.blue,
-                  ),
-
-                ],
-              );
-            }
-          );
-
-        },
-      );
-
-    },
-  ),
-)
-*/
